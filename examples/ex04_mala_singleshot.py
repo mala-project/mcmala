e132e7c40425dd904de18ac51b5102dfe60d8ec0
Lenz Fiedler <l.fiedler@hzdr.de>
2022-01-05 09:49:19 +0100

    Fixed MALA example

--- examples/ex04_mala_singleshot.py
+++ examples/ex04_mala_singleshot.py
@@ -1,12 +1,21 @@
 import os
 
 from ase.io import read
+import mcmala
 import mala
 from mala import printout
-import mcmala
 from mala.datahandling.data_repo import data_repo_path
 
 data_path = os.path.join(os.path.join(data_repo_path, "Be2"), "training_data")
+
+"""
+ex04_mala_singleshot: Shows how a Monte Carlo simulation can be done using
+MALA as the evaluator. Please not that you have to have the environment 
+variable MALA_DATA_REPO set according to the MALA setup in order for this
+example to run.
+This example first trains a small neural network, and then uses it for a
+MC run.
+"""
 
 
 # Trains a network.
@@ -88,7 +97,10 @@ def initial_training():
            data_handler.output_data_scaler
 
 
+# Perform MC run.
 def run_mc(network, params, input_scaler, output_scaler):
+    # These parameter values were not necessary for training, but are
+    # necessary for inference.
     params.targets.target_type = "LDOS"
     params.targets.ldos_gridsize = 11
     params.targets.ldos_gridspacing_ev = 2.5
@@ -98,37 +110,45 @@ def run_mc(network, params, input_scaler, output_scaler):
     params.descriptors.descriptor_type = "SNAP"
     params.descriptors.twojmax = 10
     params.descriptors.rcutfac = 4.67637
-    params.targets.pseudopotential_path = os.path.join(data_repo_path,
-                                                               "Be2")
+    params.targets.pseudopotential_path = os.path.join(data_repo_path, "Be2")
+
+    # Create data handler and with it calculator.
     inference_data_handler = mala.DataHandler(params,
                                               input_data_scaler=input_scaler,
                                               output_data_scaler=output_scaler)
-    evaluator = mala.ASECalculator(params, network,
-                                         inference_data_handler,
-                                         ["qe.out",
-                                          os.path.join(data_path,
-                                          "Be_snapshot1.out")])
+    evaluator = mala.ASECalculator(params, network, inference_data_handler,
+                                   ["qe.out",
+                                    os.path.join(data_path,
+                                                 "Be_snapshot1.out")])
 
+    # Initial configuration is one of the training snapshots.
     initial_configuration = read(os.path.join(data_path,
-                                          "Be_snapshot1.out"),
+                                              "Be_snapshot1.out"),
                                  format="espresso-out")
 
+    # Atomic displacer means one atom at a time is randomly displaced.
     suggester = mcmala.AtomDisplacer(0.2)
+
+    # We need to use the same temperature training data was calculated at.
+    # Then we can run.
     simulation = mcmala.MarkovChain(298.0, evaluator, suggester,
                                     initial_configuration)
-    simulation.run(2, print_energies=True, save_run=True)
+    simulation.run(20, print_energies=True, save_run=True)
 
 
-# network, params, input_scaler, output_scaler = initial_training()
+network, params, input_scaler, output_scaler = initial_training()
+
+# You can save and load all the MALA variables using the following commands,
+# in case you want to play around with the MC part more.
+
 # network.save_network("ex04_network.pkl")
 # params.save("ex04_params.pkl")
 # input_scaler.save("ex04_iscaler.pkl")
 # output_scaler.save("ex04_oscaler.pkl")
-
-params = mala.Parameters.load_from_file("ex04_params.pkl")
-network = mala.Network.load_from_file(params, "ex04_network.pkl")
-input_scaler = mala.DataScaler.load_from_file("ex04_iscaler.pkl")
-output_scaler = mala.DataScaler.load_from_file("ex04_oscaler.pkl")
+# params = mala.Parameters.load_from_file("ex04_params.pkl")
+# network = mala.Network.load_from_file(params, "ex04_network.pkl")
+# input_scaler = mala.DataScaler.load_from_file("ex04_iscaler.pkl")
+# output_scaler = mala.DataScaler.load_from_file("ex04_oscaler.pkl")
 
 run_mc(network, params, input_scaler, output_scaler)
 
